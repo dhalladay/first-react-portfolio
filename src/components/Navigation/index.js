f1d6a7a52a5c70dc5008afb6509ce8beaee98dd8
daveh <dave.halladay@outlook.com>
2022-03-19 01:44:53 -0600

    update Portfolio componenet

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,58 +1,21 @@
 function Navigation(props) {
-  const section = ['about', 'portfolio', 'contact', 'resume']
+  const selection = ['about', 'portfolio', 'contact', 'resume']
 
   return (
-      <nav>
-        <ul className="flex-row">
-          {section.map(section => (
-            <li className="mx-2" key={section}>
-              <a 
-                href={"#" + section}
-                onClick={() => props.setCurrentPage(section)}
-                >
-                  {section}
-              </a>
-            </li>
-          ))}
-          {/* <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-              About me
-            </a>
-          </li>
-          <li className="mx-2">
-            <a data-testid="portfolio" href="#portfolio" onClick={() => setContactSelected(true)}>
-              Portfolio
-            </a>
-          </li>
-          <li className="mx-2">
-            <a data-testid="contact" href="#contact" onClick={() => setContactSelected(true)}>
-              Contact me
-            </a>
-          </li>
-          <li className="mx-2">
-            <a data-testid="resume" href="#resume" onClick={() => setContactSelected(true)}>
-              Resume
-            </a>
-          </li> */}
-          {/* {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && `navActive`
-                }`}
-              key={category.name}
+    <nav>
+      <ul className="flex-row">
+        {selection.map(selection => (
+          <li className="mx-2" key={selection}>
+            <a
+              href={"#" + selection}
+              onClick={() => props.setCurrentPage(selection)}
             >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li> */}
-          {/* ))} */}
-        </ul>
-      </nav>
+              {selection}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
 
