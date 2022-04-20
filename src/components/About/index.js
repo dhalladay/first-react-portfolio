40c8cd742cba5e127851855cdcb1fa9c0dc9d112
daveh <dave.halladay@outlook.com>
2022-04-20 15:49:55 -0600

    prepare for new styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,15 @@
 function About() {
   return (
-    <section className="flex-row flex-wrap">
-      <div className="col-5">
+    <section className="container w-100">
+    <div className="flex-row flex-wrap">
+      <div className="col-sm-12 col-md-5">
       <img className="rounded my-2" src={require(`../../assets/images/profile-pic.jpg`)} alt="Dave laughing"></img>
       </div>
       <div className="about-text col-5">
       <h2 className="light">About me:</h2>
       <p className="light">My name is David Halladay. I am have been working in Systems Support for the last five years. 
         During that time I have had the chance to learn about logic, organization as well as various programming and scripting languages and systems and have decided to use that experince to take the next step and become a full stack developer.</p>
+      </div>
       </div>
     </section>
   );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,15 +3,15 @@ import { BsGithub } from 'react-icons/bs';
 function Projects() {
   return (
     <>
-      <section id="work" className="content container">
+      <section id="work" className="content container w-100">
         <div className="row">
           <h2 className="section-header col-2">
             Work
           </h2>
         </div>
-        <div className="col-8">
+        <div className="col-11">
           <div className="flex-row">
-            <div className="card">
+            <div className="prj-card">
               <div className='github-flex'>
                 <h3 className='my-2'>Troubleshot</h3>
                 <a href="https://github.com/Hone12345678/Troubleshot" target="_blank" rel="noreferrer" className="light link myt-2 mx-2"><BsGithub /></a>
@@ -20,7 +20,7 @@ function Projects() {
                 <img src={require(`../../assets/images/Troubleshot.png`)} alt="screenshot and link to Troubleshot website, click to proceed" className="project-img rounded" />
               </a>
             </div>
-            <div className="card">
+            <div className="prj-card">
               <div className='github-flex'>
               <h3 className="my-2">Chrip</h3>
               <a href="https://github.com/dhalladay/travel-app-events" target="_blank" rel="noreferrer" className="light link myt-2 mx-2"><BsGithub /></a>
@@ -30,7 +30,7 @@ function Projects() {
                 <img src={require(`../../assets/images/Chrip.png`)} alt="screenshot and link to Chrip website, click to proceed" className="project-img rounded" />
               </a>
             </div>
-            <div className="card">
+            <div className="prj-card">
               <div className='github-flex'>
               <h3 className="my-2">The Tech Blog</h3>
               <a href="https://github.com/dhalladay/tech-blog" target="_blank" rel="noreferrer" className="light link myt-2 mx-2"><BsGithub /></a>
@@ -40,7 +40,7 @@ function Projects() {
                 <img src={require(`../../assets/images/The-Tech-Blog.png`)} alt="screenshot and link to The Tech Blog website, click to proceed" className="project-img rounded" />
               </a>
             </div>
-            <div className="card">
+            <div className="prj-card">
               <div className='github-flex'>
               <h3 className="my-2">Work Day Scheduler</h3>
               <a href="https://github.com/dhalladay/daily-calendar" target="_blank" rel="noreferrer" className="light link myt-2 mx-2"><BsGithub /></a>
@@ -50,7 +50,7 @@ function Projects() {
                 <img src={require(`../../assets/images/Work-Day-Scheduler.png`)} alt="screenshot and link to workday scheduler application" className="project-img rounded" />
               </a>
             </div>
-            <div className="card">
+            <div className="prj-card">
               <div className='github-flex'>
               <h3 className="my-2">Run-buddy</h3>
               <a href="https://github.com/Hone12345678/worthIt" target="_blank" rel="noreferrer" className="light link myt-2 mx-2"><BsGithub /></a>
@@ -60,7 +60,7 @@ function Projects() {
                 <img src={require(`../../assets/images/WorthIt.png`)} alt="screenshot and link to run-buddy, a guided html and css example" className="project-img rounded" />
               </a>
             </div>
-            <div className="card">
+            <div className="prj-card">
               <div className='github-flex'>
               <h3 className="my-2">Weather API Dashboard</h3>
               <a href="https://github.com/dhalladay/travel-weather-dashboard" target="_blank" rel="noreferrer" className="light link myt-2 mx-2"><BsGithub /></a>
