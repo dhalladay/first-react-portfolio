031d317824b6d3e3c2a67e14e93aa14763797797
daveh <dave.halladay@outlook.com>
2022-04-25 23:34:31 -0600

    add coding bootcamp badge to resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -9,7 +9,13 @@ function Resume() {
 
   return (
     <div className="light container-fluid">
-      <h3 className="center-text">languages:</h3>
+      <h2 className="text-center">2022 Coding Bootcamp - University of Utah</h2>
+      <div className="prj-card mx-auto">
+      <a href="https://www.credly.com/badges/cf696deb-e001-4981-ba43-d6caeb52239b/public_url" target="_blank" rel="noreferrer" className="project-link">
+                <img src={require(`../../assets/images/coding-bootcamp-full-stack-developer-certificate.png`)} alt="University of Utah Coding Bootcamp certificate badge" className="project-img rounded" />
+              </a>
+      </div>
+      <h3 className="center-text">skills:</h3>
       <div className="flex-row">
         <div>
           <h4>front-end: </h4>
@@ -33,7 +39,7 @@ function Resume() {
 
         </div>
       </div>
-      <a className="light center-text my-2 resume-link" href={require(`../../assets/docs/Resume-Portfolio.pdf`)} download>Download Resume</a>
+      {/* <a className="light center-text my-2 resume-link" href={require(`../../assets/docs/Resume-Portfolio.pdf`)} download>Download Resume</a> */}
     </div>
   )
 }
