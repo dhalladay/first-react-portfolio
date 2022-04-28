ec1d8614cce9f343f63ab3fd0f0d756cf783b401
daveh <dave.halladay@outlook.com>
2022-04-27 21:45:20 -0600

    impelement emailjs and update contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useRef } from "react";
 import { validateEmail } from '../../utils/helpers';
+import emailjs from '@emailjs/browser'
 
 function ContactForm() {
+
+  const form = useRef();
+
   const [errorMessage, setErrorMessage] = useState('');
   
   const [formState, setFormState] = useState({ name: '', email: '', message: ''});
@@ -30,24 +34,29 @@ function ContactForm() {
     }
   }
 
-  function handleSubmit(e) {
+  const sendEmail = (e) => {
     e.preventDefault();
-    console.log(formState)
-  }
+    emailjs.sendForm('service_8lg2lg6', 'contact_form', form.current, 'KDEQacv3isXKEjEyA')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
 
   return (
     <section className="container-fluid">
       <div className="row">
         <div className="col-lg-6 mx-auto">
       <h1 className="light">contact me:</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
+      <form id="contact-form" ref={form} onSubmit={sendEmail}>
         <div className="my-2">
           <label htmlFor="name">name: </label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+          <input type="text" name="send_name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div className="my-2">
           <label htmlFor="email">email address: </label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+          <input type="email" name="from_email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div className="my-2">
           <label htmlFor="message">message: </label>
